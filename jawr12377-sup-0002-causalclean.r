5c8c4c44d26ff776ecaab57c1fe3ec83eb6145c0
jsta <stachel2@msu.edu>
2018-07-23 15:49:14 -0400

    turn off figure creation

--- jawr12377-sup-0002-causalclean.r
+++ jawr12377-sup-0002-causalclean.r
@@ -6,10 +6,10 @@ plotDIR <- paste(base, "manuscript", sep="/")
 require(arm)
 require(Matching)
 require(lattice)
-source("~/dropbox/rutil/util.r")
+# source("~/dropbox/rutil/util.r")
 
 ### MANAGER
-load(paste(dataDIR, "manage.RData", sep="/"))
+load("jawr12377-sup-0001-manage.rdata")
 
 ## processing data for the best phosphorus model:
 
@@ -47,8 +47,8 @@ LoadApr2007$CP <- paste(LoadApr2007$Waterway, LoadApr2007$Buffer, LoadApr2007$CF
 
 ## Figure 1 TP loading without matching
 
-postscript(file=paste(plotDIR, "with_outCP.eps", sep="/"),
-    height=5.5, width=5.5, horizontal=F)
+# postscript(file=paste(plotDIR, "with_outCP.eps", sep="/"),
+#     height=5.5, width=5.5, horizontal=F)
 par(mfcol=c(2,2), mar=c(2,3,1,1), mgp=c(1.75,0.125,0), las=1, tck=0.01)
 plot(AvgTPLoad ~ factor(AnyCP), data=LoadApr2007, log="y", axes=F, xlab="",
      ylab="Average TP Load (kg/ha/yr)")
@@ -70,15 +70,14 @@ plot(I(Avg.N.Applied+1) ~ factor(AnyCP), data=LoadApr2007, log="y", axes=F, xlab
 axis(1, at=c(1,2), labels=c("Without", "With"))
 axis(2)
 box()
-dev.off()
-
+# # dev.off()
 
 ## centering predictor
 LoadApr2007$std.PApplied <- scale(log(LoadApr2007$Avg.P.Applied+1), scale=F)
 LoadApr2007$std.NApplied <- scale(log(LoadApr2007$Avg.N.Applied+1), scale=F)
-LoadApr2007$std.Runoff <- scale(log(LoadApr2007$Avg.Runoff), scale=F)
-LoadApr2007$std.SoilL <- scale(log(LoadApr2007$Avg.SoilL+1), scale=F)
-LoadApr2007$std.TPLoad <- scale(log(LoadApr2007$AvgTPLoad), scale=F)
+LoadApr2007$std.Runoff   <- scale(log(LoadApr2007$Avg.Runoff), scale=F)
+LoadApr2007$std.SoilL    <- scale(log(LoadApr2007$Avg.SoilL+1), scale=F)
+LoadApr2007$std.TPLoad   <- scale(log(LoadApr2007$AvgTPLoad), scale=F)
 
 ## matching
 clean.data <- LoadApr2007[,  c("AnyCP", "yp","std.PApplied","std.Runoff","std.SoilL",
@@ -93,7 +92,7 @@ pscores1 <- predict(ps.m1, type="link")
 matches1 <- matching(z=clean.data$AnyCP, score=pscores1)
 
 matched1 <- clean.data[matches1$matched, ]
-b.stats <- balance(clean.data, matched1, ps.m1)
+b.stats  <- balance(clean.data, matched1, ps.m1)
 #print(b.stats)
 plot(b.stats)
 by(matched1$yp, matched1$AnyCP, mean)
@@ -101,7 +100,7 @@ by(clean.data$yp, clean.data$AnyCP, mean)
 
 
 ## alternative methods -- may lead to different matched subset, hence, different effect
-### 1. using package Matching: 
+### 1. using package Matching:
 rr1 <- Match(Y=clean.data$yp, Tr=clean.data$AnyCP, X=ps.m1$fitted)
 summary(rr1)
 ## a smaller effect
@@ -119,8 +118,8 @@ by(matched2$yp, matched2$AnyCP, mean)
 
 ## Figure 3 before and after matching
 ctr <- attr(LoadApr2007$std.PApplied, "scaled:center")
-postscript(file=paste(plotDIR, "psmatching.eps", sep="/"),
-           width=5, height=2.5, horizontal=F)
+# postscript(file=paste(plotDIR, "psmatching.eps", sep="/"),
+#            width=5, height=2.5, horizontal=F)
 par(mfrow=c(1,2),tck=0.01,las=1,mgp=c(1.5,0.125,0))
 par(mar=c(2, 3, 2,0.25))
 plot(std.PApplied~factor(AnyCP), data=clean.data, names=c("Without","With"),
@@ -136,7 +135,7 @@ axis(1, at=c(1,2), labels=c("Without","With"))
 axis(4, at=log(c(0,5,10,50,100,250)+1)-ctr, labels=c(0,5,10,50,100,250))
 box()
 title(main="after matching", cex=0.75)
-dev.off()
+# # dev.off()
 ##
 
 ## regression after matching
@@ -239,13 +238,13 @@ plot.data <- plot.data[temp,]
 plot.data$LU <- ordered(as.character(plot.data$LU))
 
 ## figure 3
-trellis.device(postscript, file=paste(plotDIR, "manageEff1.eps", sep="/"),
-               width=4.5, height=5, horizontal=F)
-trellis.par.set(theme = canonical.theme("postscript", col=FALSE))
-trellis.par.set(list(fontsize=list(text=8),
-                 par.xlab.text=list(cex=1.25),
-                     add.text=list(cex=1.25),
-                     superpose.symbol=list(cex=1)))
+# trellis.device(postscript, file=paste(plotDIR, "manageEff1.eps", sep="/"),
+#                width=4.5, height=5, horizontal=F)
+# trellis.par.set(theme = canonical.theme("postscript", col=FALSE))
+# trellis.par.set(list(fontsize=list(text=8),
+#                  par.xlab.text=list(cex=1.25),
+#                      add.text=list(cex=1.25),
+#                      superpose.symbol=list(cex=1)))
 key <- simpleKey(unique(plot.data$CP1), lines=T, points=F, space = "top", columns=2)
 key$text$cex <- 1.25
 xyplot(y ~ x|LU, data=plot.data, type="l",
@@ -255,15 +254,15 @@ xyplot(y ~ x|LU, data=plot.data, type="l",
         panel.grid()
     }, layout=c(3,3)
     )
-dev.off()
+# dev.off()
 
 ## data points (not used in the paper)
-trellis.device(postscript, file="manageEff12.eps", width=4.5, height=5, horizontal=F)
-trellis.par.set(theme = canonical.theme("postscript", col=FALSE))
-trellis.par.set(list(fontsize=list(text=8),
-                 par.xlab.text=list(cex=1.25),
-                     add.text=list(cex=1.25),
-                     superpose.symbol=list(cex=1)))
+# trellis.device(postscript, file="manageEff12.eps", width=4.5, height=5, horizontal=F)
+# trellis.par.set(theme = canonical.theme("postscript", col=FALSE))
+# trellis.par.set(list(fontsize=list(text=8),
+#                  par.xlab.text=list(cex=1.25),
+#                      add.text=list(cex=1.25),
+#                      superpose.symbol=list(cex=1)))
 key <- simpleKey(unique(plot.data$CP1), lines=F, points=T, space = "top", columns=2)
 key$text$cex <- 1.25
 xyplot(yp ~ x|LU, data=plot.data,type="p",
@@ -277,7 +276,7 @@ xyplot(yp ~ x|LU, data=plot.data,type="p",
     }#,
     #scales=list(x=list(at=log(c(0, 10, 50, 100, 500, 1000)+1), labels=as.character(c(0, 10, 50, 100, 500, 1000))))
     )
-dev.off()
+# dev.off()
 
 ## other models tried
 M10.2 <- lmer(yp ~ std.PApplied + std.Runoff+ std.SoilL+std.PApplied*AnyCP+
@@ -323,13 +322,13 @@ M10.6 <- lmer(yp ~ std.PApplied + std.Runoff+ std.SoilL+ (1+std.PApplied|CP)+
             data=LoadApr2007, subset=xp>0)
 
 ## Figure 2
-postscript(file=paste(plotDIR, "mlindCPs.eps", sep="/"),
-           height=4, width=4.5, horizontal=F)
+# postscript(file=paste(plotDIR, "mlindCPs.eps", sep="/"),
+#            height=4, width=4.5, horizontal=F)
 par(mfrow=c(1,2), mgp =c(1.5,.5,0), mar=c(3, 5, 1, 0.25))
 plot.lmer.ranef(M=M10.6, ranV=2, column=1, xlab="Intercept", ylab=" ", column.fixed=1, y.cex=0.7)
 par(mar=c(3, 0.25, 1, 5))
 plot.lmer.ranef(M=M10.6, ranV=2, column=2, xlab="log P Applied", ylab=" ", yaxis=4, column.fixed=2, y.cex=0.7)
-dev.off()
+# dev.off()
 ##
 
 ## other exploratory plots
